22dbccc75bbad3381fca1df04b12f49e0fd38a29
Antony-SS <jns5@illinois.edu>
2022-11-10 18:40:23 -0600

    waiting on this until we get an API Key

--- contract/scripts/nftbankAPI.py
+++ contract/scripts/nftbankAPI.py
@@ -4,19 +4,5 @@ apikey = "TBD"
 
 
 
-# Alchemy queries openSea and LooksRare
+# WAITING ON THIS UNITL WE GET API KEY
 
-
-def get_nft_floor_price_alchemy(address: str):
-    url = "https://eth-mainnet.g.alchemy.com/nft/v2/" + apikey + "/getFloorPrice?contractAddress=" + address
-    headers = {"accept": "application/json"}
-
-    response = requests.get(url, headers=headers)
-    data = response.json()
-
-    if "openSea" in data:
-        return(float(data['openSea']['floorPrice']))
-    else:
-        return(float(data['looksRare']['floorPrice']))
-
-print (get_nft_floor_price_alchemy("0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"))
